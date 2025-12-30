4f5ff6e1e7ef2f26c1862239074d1b80d4989592
Harishma Manoharan <harishmahari1502@gmail.com>
2025-12-30 14:07:29 +0530

    File data changed

--- app.py
+++ app.py
@@ -449,7 +449,5 @@ def upload():
 
 # ---------------- MAIN ----------------
 if __name__ == "__main__":
-    init_db()
-    app.run(debug=True)
-
-
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
